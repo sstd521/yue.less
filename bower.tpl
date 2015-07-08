636270e310b97e60f3c8567333cef63342f3d4f5
mamboer <mamboer@live.com>
2015-07-08 13:38:14 +0800

    v1.0.0

---
+++ bower.tpl
@@ -0,0 +1,34 @@
+{
+  "name": "@@name",
+  "version": "@@version",
+  "homepage": "https://github.com/mamboer/@@name",
+  "authors": "@@author",
+  "description": "@@description",
+  "main": @@main,
+  "keywords": "@@keywords",
+  "license": "MIT",
+  "dependencies":{
+    "classor":"~1.0.0",
+    "Snap.svg":"*"    
+  },
+  "ignore": [
+    "gulpfile.js",
+    "*.swig",
+    "*.tpl",
+    "*.jst",
+    "README.html",
+    "img",
+    "grunt",
+    "src",
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "dist/vendors/classor",
+    "dist/vendors/Snap.svg",
+    "test",
+    "tests",
+    "*.json",
+    "site",
+    "*.html"
+  ]
+}
--- grunt/replace.js
+++ grunt/replace.js
@@ -16,6 +16,18 @@ module.exports = {
           replacement: '<%= package.keywords %>'
         },
         {
+          match: "author",
+          replacement: '<%= package.author.name %>'
+        },
+        {
+          match: "main",
+          replacement: '<%= package.main %>'
+        },
+        {
+          match: "name",
+          replacement: '<%= package.name %>'
+        },
+        {
           match: "title",
           replacement: '<%= package.title %>'
         }
@@ -23,6 +35,7 @@ module.exports = {
     },
     files: [
       {expand: false, flatten: true, src: ['README.html'], dest: 'index.html'},
+      {expand: false, flatten: true, src: ['bower.tpl'], dest: 'bower.json'}
     ]
   }
 };
